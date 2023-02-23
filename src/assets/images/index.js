70d47b2fe5ae2fff6b365bf649af92b2d0392581
Vankhiem <vankhiem@gmail.com>
2023-02-23 15:15:16 +0700

    header UI #1

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,6 @@
 const images = {
   logo: require('~/assets/images/logo.svg').default,
-  noImg: require('~/assets/images/fallback-avatar.155cdb2376c5d99ea151.jpg'),
+  noImg: require('~/assets/images/fallback-avatar.jpg'),
 };
 
 export default images;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import images from '~/assets/images';
 
 const StyledHeader = styled.header`
+  /* variable css */
+  --search-border-radius: 92px;
+  --search-height: 46px;
+  --search-top-spacer: 9px;
+  --search-button-width: 52px;
+  /* ------------- */
   height: var(--default-layout-header-height);
   width: 100%;
   box-shadow: 0px 1px 1px rgb(0 0 0 / 12%);
@@ -10,13 +19,89 @@ const StyledHeader = styled.header`
   .inner {
     height: 100%;
     width: var(--default-layout-width);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .search {
+      width: 361px;
+      height: var(--search-height);
+      background-color: rgba(241, 241, 242, 1);
+      border-radius: var(--search-border-radius);
+      padding-left: 16px;
+      display: flex;
+      position: relative;
+      border: 1.5px solid transparent;
+      &::after {
+        content: '';
+        position: absolute;
+        height: calc(var(--search-height) - var(--search-top-spacer) * 2);
+        background-color: rgba(22, 24, 35, 0.12);
+        width: 1px;
+        right: var(--search-button-width);
+        top: var(--search-top-spacer);
+      }
+      &:focus-within {
+        border: 1.5px solid rgba(22, 24, 35, 0.2);
+      }
+      input {
+        flex: 1;
+        background-color: rgba(241, 241, 242, 1);
+        height: 100%;
+        color: var(--black);
+        font-size: 1.6rem;
+        caret-color: ${(props) => props.theme.colors.primary};
+
+        &:not(:placeholder-shown) ~ .search-btn {
+          color: rgba(22, 24, 35, 0.75);
+        }
+      }
+      .clear,
+      .loading {
+        font-size: 1.6rem;
+        position: absolute;
+        right: calc(var(--search-button-width) + 16px);
+        top: 50%;
+        transform: translateY(-50%);
+        color: rgba(22, 24, 35, 0.34);
+      }
+      .search-btn {
+        width: var(--search-button-width);
+        height: 100%;
+        border-top-right-radius: var(--search-border-radius);
+        border-bottom-right-radius: var(--search-border-radius);
+        font-size: 1.8rem;
+        color: rgba(22, 24, 35, 0.34);
+        &:hover {
+          background-color: rgba(22, 24, 35, 0.03);
+          cursor: pointer;
+        }
+        &:active {
+          background-color: rgba(22, 24, 35, 0.06);
+        }
+      }
+    }
   }
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      <div className="inner"></div>
+      <div className="inner">
+        <div className="logo">
+          <img src={images.logo} alt="tiktok" />
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search accounts and videos" spellCheck="false" />
+          <button className="clear">
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className="loading" icon={faSpinner} />
+          <button className="search-btn">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className="actions"></div>
+      </div>
     </StyledHeader>
   );
 }
